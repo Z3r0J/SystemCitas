0cdb164718c38c2c136ca902e1fb6df7cd52c039
Jean Carlos Reyes Encarnación <20210546@itla.edu.do>
2022-04-21 15:56:34 -0400

    FrmLogin Change Name

--- SystemCitas/FrmLogin.Designer.cs
+++ SystemCitas/FrmLogin.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace SystemCitas
 {
-    partial class Form1
+    partial class FrmLogin
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,10 +29,17 @@ namespace SystemCitas
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(401, 302);
+            this.Name = "FrmLogin";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- SystemCitas/Program.cs
+++ SystemCitas/Program.cs
@@ -17,7 +17,7 @@ namespace SystemCitas
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmLogin());
         }
     }
 }
