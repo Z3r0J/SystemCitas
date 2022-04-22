cde3c1195ca782620dd4b7b3617d2c584a1e4291
Johanly Baez <20209409@itla.edu.do>
2022-04-21 21:56:40 -0400

    Cambios Loginsss

--- SystemCitas/FrmLogin.cs
+++ SystemCitas/FrmLogin.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,11 @@ namespace SystemCitas
 {
     public partial class FrmLogin : Form
     {
+        public string connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
         public FrmLogin()
         {
             InitializeComponent();
+            SqlConnection connection = new SqlConnection(connectionString);
         }
 
     }
