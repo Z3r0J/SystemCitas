fa8e1d0b87d2e9e38068b080a08e7d4ad4c2a1a8
Johanly Baez <20209409@itla.edu.do>
2022-04-22 02:19:26 -0400

    Nuevos Forms

--- SystemCitas/DatosCitas.Designer.cs
+++ SystemCitas/DatosCitas.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SystemCitas
 {
-    partial class DoctorData
+    partial class DatosCitas
     {
         /// <summary>
         /// Required designer variable.
@@ -28,22 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DoctorData));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DatosCitas));
             this.panel1 = new System.Windows.Forms.Panel();
             this.btnBuscar = new System.Windows.Forms.Button();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.txtBuscarDoctor = new System.Windows.Forms.TextBox();
             this.btnCerrar = new System.Windows.Forms.Button();
-            this.btnEliminar = new System.Windows.Forms.Button();
-            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnCompletar = new System.Windows.Forms.Button();
             this.btnAgregar = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.dataDotores = new System.Windows.Forms.DataGridView();
+            this.dataPacientes = new System.Windows.Forms.DataGridView();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataDotores)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataPacientes)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,16 +53,15 @@
             this.panel1.Controls.Add(this.pictureBox3);
             this.panel1.Controls.Add(this.txtBuscarDoctor);
             this.panel1.Controls.Add(this.btnCerrar);
-            this.panel1.Controls.Add(this.btnEliminar);
-            this.panel1.Controls.Add(this.btnEditar);
+            this.panel1.Controls.Add(this.btnCompletar);
             this.panel1.Controls.Add(this.btnAgregar);
             this.panel1.Controls.Add(this.pictureBox2);
-            this.panel1.Controls.Add(this.dataDotores);
+            this.panel1.Controls.Add(this.dataPacientes);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(700, 492);
-            this.panel1.TabIndex = 0;
+            this.panel1.Size = new System.Drawing.Size(701, 496);
+            this.panel1.TabIndex = 3;
             // 
             // btnBuscar
             // 
@@ -78,7 +76,7 @@
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Image = global::SystemCitas.Properties.Resources.doctor_128_44166;
+            this.pictureBox3.Image = global::SystemCitas.Properties.Resources.medical_14_icon_icons_com_73923;
             this.pictureBox3.Location = new System.Drawing.Point(394, 3);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(169, 124);
@@ -99,49 +97,38 @@
             // 
             this.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCerrar.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnCerrar.Location = new System.Drawing.Point(585, 450);
+            this.btnCerrar.Location = new System.Drawing.Point(585, 431);
             this.btnCerrar.Name = "btnCerrar";
-            this.btnCerrar.Size = new System.Drawing.Size(89, 28);
+            this.btnCerrar.Size = new System.Drawing.Size(89, 38);
             this.btnCerrar.TabIndex = 11;
             this.btnCerrar.Text = "Cerrar";
             this.btnCerrar.UseVisualStyleBackColor = true;
             // 
-            // btnEliminar
+            // btnCompletar
             // 
-            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnEliminar.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnEliminar.Location = new System.Drawing.Point(585, 373);
-            this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(89, 28);
-            this.btnEliminar.TabIndex = 10;
-            this.btnEliminar.Text = "Eliminar";
-            this.btnEliminar.UseVisualStyleBackColor = true;
-            // 
-            // btnEditar
-            // 
-            this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnEditar.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnEditar.Location = new System.Drawing.Point(585, 290);
-            this.btnEditar.Name = "btnEditar";
-            this.btnEditar.Size = new System.Drawing.Size(89, 28);
-            this.btnEditar.TabIndex = 9;
-            this.btnEditar.Text = "Editar";
-            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnCompletar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCompletar.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCompletar.Location = new System.Drawing.Point(585, 323);
+            this.btnCompletar.Name = "btnCompletar";
+            this.btnCompletar.Size = new System.Drawing.Size(89, 40);
+            this.btnCompletar.TabIndex = 9;
+            this.btnCompletar.Text = "Completar Citas";
+            this.btnCompletar.UseVisualStyleBackColor = true;
             // 
             // btnAgregar
             // 
             this.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAgregar.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnAgregar.Location = new System.Drawing.Point(585, 206);
+            this.btnAgregar.Location = new System.Drawing.Point(585, 220);
             this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(89, 28);
+            this.btnAgregar.Size = new System.Drawing.Size(89, 38);
             this.btnAgregar.TabIndex = 8;
             this.btnAgregar.Text = "Agregar";
             this.btnAgregar.UseVisualStyleBackColor = true;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Image = global::SystemCitas.Properties.Resources.Doctor_Female_icon_icons_com_75050;
+            this.pictureBox2.Image = global::SystemCitas.Properties.Resources.crowd_patient_patients_icon_140474;
             this.pictureBox2.Location = new System.Drawing.Point(202, 3);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(155, 124);
@@ -149,19 +136,19 @@
             this.pictureBox2.TabIndex = 6;
             this.pictureBox2.TabStop = false;
             // 
-            // dataDotores
+            // dataPacientes
             // 
-            this.dataDotores.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.dataDotores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataDotores.Location = new System.Drawing.Point(3, 133);
-            this.dataDotores.Name = "dataDotores";
-            this.dataDotores.RowTemplate.Height = 25;
-            this.dataDotores.Size = new System.Drawing.Size(560, 345);
-            this.dataDotores.TabIndex = 5;
+            this.dataPacientes.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.dataPacientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataPacientes.Location = new System.Drawing.Point(3, 133);
+            this.dataPacientes.Name = "dataPacientes";
+            this.dataPacientes.RowTemplate.Height = 25;
+            this.dataPacientes.Size = new System.Drawing.Size(560, 345);
+            this.dataPacientes.TabIndex = 5;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::SystemCitas.Properties.Resources.doctor_icon_icons_com_76230;
+            this.pictureBox1.Image = global::SystemCitas.Properties.Resources.medical_42_icon_icons_com_73897;
             this.pictureBox1.Location = new System.Drawing.Point(3, 3);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(165, 124);
@@ -169,24 +156,23 @@
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
-            // DoctorData
+            // DatosCitas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.ClientSize = new System.Drawing.Size(724, 516);
+            this.ClientSize = new System.Drawing.Size(725, 520);
             this.ControlBox = false;
             this.Controls.Add(this.panel1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "DoctorData";
+            this.Name = "DatosCitas";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "SystemCitas | Doctores";
+            this.Text = "SystemCitas | Citas";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataDotores)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataPacientes)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -195,15 +181,14 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.DataGridView dataDotores;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button btnCerrar;
-        private System.Windows.Forms.Button btnEliminar;
-        private System.Windows.Forms.Button btnEditar;
-        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnBuscar;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.TextBox txtBuscarDoctor;
-        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button btnCompletar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.DataGridView dataPacientes;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- SystemCitas/Program.cs
+++ SystemCitas/Program.cs
@@ -17,7 +17,7 @@ namespace SystemCitas
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new RegistroCitas());
+            Application.Run(new DatosUsuarios());
         }
     }
 }
