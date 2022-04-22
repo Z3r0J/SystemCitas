dc1249f9e7f7571a2344d79ff7ce3d9cf0de1844
Jean Carlos Reyes Encarnación <20210546@itla.edu.do>
2022-04-21 21:08:43 -0400

    ConnectionString SQL

--- BusinessLayer/BusinessDoctor.cs
+++ BusinessLayer/BusinessDoctor.cs
@@ -1,9 +1,15 @@
-﻿using System;
+﻿using DataLayer;
+using System;
+using System.Data.SqlClient;
 
 namespace BusinessLayer
 {
     public class BusinessDoctor
     {
-         
+        private DataDoctor dataDoctor;
+        public BusinessDoctor(SqlConnection connection)
+        {
+            dataDoctor = new DataDoctor(connection);
+        }
     }
 }
--- DataLayer/DataDoctor.cs
+++ DataLayer/DataDoctor.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace DataLayer
 {
     public class DataDoctor
     {
-
+        private SqlConnection _conexion;
+        public DataDoctor(SqlConnection connection)
+        {
+            _conexion = connection;
+        }
     }
 }
